f0f2b20cb6ebde383aeefbc417f71af1ef1999c6
kokojae <54296776+kokojae@users.noreply.github.com>
2020-03-26 20:08:29 +0900

    ObjectManager 틀 완성
    ObjectManager 틀 완성함 GameObject완성후에 마저 완성해야함

---
+++ ObjectManager.cpp
@@ -0,0 +1,33 @@
+#include "DXUT.h"
+#include "ObjectManager.h"
+
+std::list<GameObject*> ObjectManager::obj_list = std::list<GameObject*>();
+
+void ObjectManager::Sort(const GameObject* a, const GameObject* b)
+{
+}
+
+void ObjectManager::Update()
+{
+}
+
+void ObjectManager::LateUpdate()
+{
+}
+
+void ObjectManager::Render()
+{
+}
+
+void ObjectManager::Release()
+{
+}
+
+void ObjectManager::Clear()
+{
+}
+
+GameObject* ObjectManager::ColliderCheck(RECT* objRc, int layer)
+{
+	return nullptr;
+}
---
+++ ObjectManager.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <list>
+#include "Layer.h"
+class GameObject;
+
+class ObjectManager
+{
+public:
+	static std::list<GameObject*> obj_list;
+
+	template <typename T>
+	static T* Instantiate(D3DXVECTOR2 pos = { 0,0 });
+	void Sort(const GameObject* a, const GameObject* b);
+
+	static void Update();
+	static void LateUpdate();
+	static void Render();
+	static void Release();
+	static void Clear();
+
+	static GameObject* ColliderCheck(RECT* objRc, int layer = Layer::DEFAULT);
+};
+
+template<typename T>
+inline T* ObjectManager::Instantiate(D3DXVECTOR2 pos)
+{
+	T* inst = new T;
+
+	obj_list.push_back(inst);
+	inst->Init();
+	inst->pos = pos;
+	obj_list.sort(Sort);
+
+	return inst;
+}
