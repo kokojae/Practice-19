42a9b40802ddb715c63b54d3acdb00adf90084a0
kokojae <54296776+kokojae@users.noreply.github.com>
2020-03-28 22:10:16 +0900

    GameObject 완성
    모든 오브젝트의 기본이 되는 GameObject 클래스 완성

---
+++ GameObject.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include "TextureInfo.h"
+#include "BoxCollider.h"
+#include "Layer.h"
+#include "MouseManager.h"
+
+class GameObject
+{
+public:
+	D3DXVECTOR2 pos = { 0,0 };
+	TextureInfo info;
+	BoxCollider collider;
+	bool destroy = false;
+	int z = 0;
+	RECT* rc = new RECT();
+
+	virtual void Init();
+	virtual void Update();
+	virtual void LateUpdate();
+	virtual void Render();
+	virtual void Release();
+
+	void DrawSelf();
+	GameObject* PlaceMeeting(D3DXVECTOR2 vec, int layer = Layer::DEFAULT);
+	bool IsClicked(MouseManager::Key key = MouseManager::Key::ANY);
+
+	void SetObject(std::wstring path, D3DXVECTOR2 tex_size, D3DXVECTOR2 col_size, D3DXVECTOR2 obj_scale);
+	void SetTexture(std::wstring path, D3DXVECTOR2 size, D3DXVECTOR2 scale);
+	void SetCollider(D3DXVECTOR2 size, D3DXVECTOR2 scale);
+
+	RECT* GetRect(D3DXVECTOR2 vector = { 0,0 });
+};
+
