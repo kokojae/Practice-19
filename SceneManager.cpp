4e4e65e4cefb614bb1780277d077b6c4de4a3bb3
kokojae <54296776+kokojae@users.noreply.github.com>
2020-03-29 22:07:10 +0900

    SceneManager 추가
    SceneManager 추가 및 기틀 마련 이제 게임 오브젝트들 만들고 배치하면됨

---
+++ SceneManager.cpp
@@ -0,0 +1,36 @@
+#include "DXUT.h"
+#include "SceneManager.h"
+#include "Practice-19.h"
+
+SceneManager::Scene SceneManager::current_scene = SceneManager::Scene();
+
+void SceneManager::LoatScene(Scene scene)
+{
+	current_scene = scene;
+
+	Camera::pos = { 0,0 };
+	Camera::degree = 0;
+	Camera::scale = 1;
+	Camera::shake = { 0,0 };
+
+	ObjectManager::Clear();
+	SoundManager::EndAllSFX();
+
+	switch (scene)
+	{
+	case SceneManager::Scene::Intro:
+		break;
+	case SceneManager::Scene::Main:
+		break;
+	case SceneManager::Scene::HowTo:
+		break;
+	case SceneManager::Scene::Rank:
+		break;
+	case SceneManager::Scene::Credit:
+		break;
+	case SceneManager::Scene::Stage1:
+		break;
+	case SceneManager::Scene::Stage2:
+		break;
+	}
+}
---
+++ SceneManager.h
@@ -0,0 +1,11 @@
+#pragma once
+class SceneManager
+{
+public:
+	static enum class Scene { Intro, Main, HowTo, Rank, Credit, Stage1, Stage2 };
+
+	static Scene current_scene;
+
+	static void LoatScene(Scene scene);
+};
+
