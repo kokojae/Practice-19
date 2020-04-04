ae44e8ac9a692036b254007cbc2b067c8fef2c7e
kokojae <54296776+kokojae@users.noreply.github.com>
2020-04-04 21:05:03 +0900

    무엇
    커밋했는데 남아있네

---
+++ Player.cpp
@@ -0,0 +1,47 @@
+#include "DXUT.h"
+#include "Player.h"
+#include "Practice-19.h"
+
+void Player::Init()
+{
+	SetObject(L"resorce/player/player1.png", { 300,150 }, { 300,150 });
+	level1 = TextureManager::LoadTexture(L"resorce/player/player1.png");
+	level2 = TextureManager::LoadTexture(L"resorce/player/player2.png");
+	level3 = TextureManager::LoadTexture(L"resorce/player/player3.png");
+
+	z = 10;
+	collider.layer = Layer::PLAYER;
+	name = L"player";
+
+	cam_pos = pos;
+
+	ObjectManager::Instantiate<Text>(pos)->SetText(L"0");
+}
+
+void Player::Update()
+{
+	Gravity();
+	Input();
+	AddForce();
+}
+
+void Player::LateUpdate()
+{
+	CameraFollow();
+}
+
+void Player::Input()
+{
+}
+
+void Player::CameraFollow()
+{
+	cam_pos += (pos - cam_pos) / 30;
+
+	Camera::pos = cam_pos + D3DXVECTOR2(
+		rand() % static_cast<int>(Camera::shake.x) - static_cast<int>(Camera::shake.x / 2),
+		rand() % static_cast<int>(Camera::shake.y) - static_cast<int>(Camera::shake.y / 2)
+	);
+
+	Camera::scale += (1 - Camera::scale) / 10;
+}
---
+++ SceneManager.cpp
@@ -0,0 +1,38 @@
+#include "DXUT.h"
+#include "SceneManager.h"
+#include "Practice-19.h"
+
+SceneManager::Scene SceneManager::current_scene = SceneManager::Scene();
+
+void SceneManager::LoadScene(Scene scene)
+{
+	current_scene = scene;
+
+	Camera::pos = { 0,0 };
+	Camera::degree = 0;
+	Camera::scale = 1;
+
+	ObjectManager::Clear();
+	SoundManager::EndAllSFX();
+
+	switch (scene)
+	{
+	case SceneManager::Scene::Intro:
+		ObjectManager::Instantiate<Player>();
+		break;
+	case SceneManager::Scene::Main:
+		break;
+	case SceneManager::Scene::HowTo:
+		break;
+	case SceneManager::Scene::Rank:
+		break;
+	case SceneManager::Scene::Credit:
+		break;
+	case SceneManager::Scene::Stage1:
+		break;
+	case SceneManager::Scene::Stage2:
+		break;
+	}
+
+	GameManager::player = ObjectManager::SearchObject(L"player");
+}
